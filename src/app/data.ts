9878fdffa3b2774849867ee564a697220e03ce5b
kotala-sudo <pkotala@yahoo.com>
2022-03-09 09:15:37 -0600

    Initial commit

---
+++ src/app/data.ts
@@ -0,0 +1,23 @@
+PRODUCTS: [] = [
+  {
+    productID: '1',
+    productName: 'Television',
+    price: '800.00',
+  },
+
+  {
+    productID: '2',
+    productName: 'Blueray Player',
+    price: '200.00',
+  },
+  {
+    productID: '3',
+    productName: 'Game Console',
+    price: '300.00',
+  },
+  {
+    productID: '4',
+    productName: 'Mobile Phone',
+    price: '400.00',
+  },
+];
